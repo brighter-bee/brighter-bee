11dc8655038f4d7eca1b38cc049c5dd9a1e1f64e
Yifan Ai <ayfallen@hotmail.com>
2020-07-17 01:55:18 +1000

    Update admin site

--- forastudent/person/admin.py
+++ forastudent/person/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Skill)
+admin.site.register(Opportunity)
+admin.site.register(Person)
+admin.site.register(Meeting)
+admin.site.register(Post)
+admin.site.register(Reply)
+
