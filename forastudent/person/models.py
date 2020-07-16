69b8286f1a718a00b3d4aebb34264ff838dfc2e2
Yifan Ai <ayfallen@hotmail.com>
2020-07-17 01:42:46 +1000

    Implement db

--- forastudent/person/models.py
+++ forastudent/person/models.py
@@ -1,3 +1,87 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + " - " + self.category.name
+
+
+class Opportunity(models.Model):
+    TYPES = (
+        ('J', 'Job'),
+        ('G', 'Graduate'),
+        ('I', 'Internship'),
+        ('R', 'Research'),
+        ('O', 'Others')
+    )
+    type = models.TextField(choices=TYPES)
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    link = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + " - " + self.type
+
+
+class Person(models.Model):
+    TYPES = (
+        'Student',
+        'Professional',
+        'Organization',
+        'Others'
+    )
+    type = models.TextField()
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    location = models.TextField()
+    avatar = models.ImageField(upload_to='images/', default='images/avatar.jpg')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    skills = models.ManyToManyField(Skill)
+    opportunities = models.ManyToManyField(Opportunity)
+
+    def __str__(self):
+        return self.name + " - " + self.user.username
+
+    def username(self):
+        return self.user.username
+
+
+class Meeting(models.Model):
+    name = models.CharField(max_length=100)
+    number = models.TextField()
+    time = models.DateTimeField()
+    participants = models.ManyToManyField(Person)
+
+
+class AbstractPost(models.Model):
+    isOriginal = models.BooleanField()
+    content = models.TextField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    poster = models.ForeignKey(Person, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class Post(AbstractPost):
+    name = models.CharField(max_length=100)
+
+
+class Reply(AbstractPost):
+    replyTo = models.ForeignKey(Post, on_delete=models.CASCADE)
