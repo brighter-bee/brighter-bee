b7d88385d9dea2a5335bb83558eda700a9acd14a
Yifan Ai <ayfallen@hotmail.com>
2020-07-17 02:15:53 +1000

    Correct additional plurals

--- forastudent/person/models.py
+++ forastudent/person/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 
 class Skill(models.Model):
     name = models.CharField(max_length=100)
