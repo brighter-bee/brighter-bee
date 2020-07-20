4e8ca9af52f9e9ad9f601845bde0b735ef376430
Yifan Ai <ayfallen@hotmail.com>
2020-07-20 22:32:41 +1000

    Fine grain user types

--- forastudent/person/models.py
+++ forastudent/person/models.py
@@ -104,3 +104,13 @@ class Reply(AbstractPost):
 
     class Meta:
         verbose_name_plural = 'Replies'
+
+
+class Course(models.Model):
+    code = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    link = models.TextField(blank=True, null=True)
+    skills = models.ManyToManyField(Skill, blank=True)
+
+    def __str__(self):
+        return self.code + " " + self.name
