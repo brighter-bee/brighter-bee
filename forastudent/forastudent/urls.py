1d8ffe8af2c7aed8af6bea3e5a001688a5227c55
Yifan Ai <ayfallen@hotmail.com>
2020-07-23 00:05:56 +1000

    Add more API endpoints

--- forastudent/forastudent/urls.py
+++ forastudent/forastudent/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 
 import person.api_views
@@ -24,6 +24,9 @@ urlpatterns = [
     path('api/v2/persons', person.api_views.PersonList.as_view()),
     path('api/v2/meetings', person.api_views.MeetingList.as_view()),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/v2/meetings/new', person.api_views.MeetingCreate.as_view()),
+    path('api/v2/meetings/<int:id>/', person.api_views.MeetingRetrieveUpdateDestroy.as_view()),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- forastudent/person/api_views.py
+++ forastudent/person/api_views.py
@@ -1,9 +1,12 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import *
 from .models import *
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.exceptions import ValidationError
+from django.utils import timezone
+from django.core.cache import cache
 
 
 class GeneralPagination(LimitOffsetPagination):
@@ -27,3 +30,40 @@ class MeetingList(ListAPIView):
     filter_fields = ('id', 'number', 'participants', 'time')
     search_fields = ('name',)
     pagination_class = GeneralPagination
+
+
+class MeetingCreate(CreateAPIView):
+    serializer_class = MeetingSerializer
+
+    def create(self, request, *args, **kwargs):
+        time = request.data.get('time')
+        # now = timezone.now()
+        # if time < now:
+        #     raise ValidationError({'time': 'Must be in the future'})
+        #
+        return super().create(request, *args, **kwargs)
+
+
+class MeetingRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    queryset = Meeting.objects.all()
+    lookup_field = 'id'
+    serializer_class = MeetingSerializer
+
+    def delete(self, request, *args, **kwargs):
+        mid = request.data.get('id')
+        response = super().delete(request, *args, **kwargs)
+        if response.status_code == 204:
+            cache.delete(f'meeting_data_{mid}')
+        return response
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        if response.status_code == 200:
+            meeting = response.data
+            cache.set(f"meeting_data_{meeting['id']}", {
+                'name': meeting['name'],
+                'time': meeting['time'],
+                'number': meeting['number'],
+                'participants': meeting['participants'],
+            })
+        return response
