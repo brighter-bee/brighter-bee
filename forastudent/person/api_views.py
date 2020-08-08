950fab5f18cfa7a92f298f1857afcebc134298dc
Yifan Ai <ayfallen@hotmail.com>
2020-08-08 21:35:02 +1000

    Simplify API logic for meetings (as an example)

--- forastudent/person/api_views.py
+++ forastudent/person/api_views.py
@@ -52,38 +52,11 @@ class MeetingList(ListAPIView):
 class MeetingCreate(CreateAPIView):
     serializer_class = MeetingSerializer
 
-    def create(self, request, *args, **kwargs):
-        time = request.data.get('time')
-        # now = timezone.now()
-        # if time < now:
-        #     raise ValidationError({'time': 'Must be in the future'})
-        #
-        return super().create(request, *args, **kwargs)
-
 
 class MeetingRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     queryset = Meeting.objects.all()
     lookup_field = 'id'
     serializer_class = MeetingSerializer
-
-    def delete(self, request, *args, **kwargs):
-        mid = request.data.get('id')
-        response = super().delete(request, *args, **kwargs)
-        if response.status_code == 204:
-            cache.delete(f'meeting_data_{mid}')
-        return response
-
-    def update(self, request, *args, **kwargs):
-        response = super().update(request, *args, **kwargs)
-        if response.status_code == 200:
-            meeting = response.data
-            cache.set(f"meeting_data_{meeting['id']}", {
-                'name': meeting['name'],
-                'time': meeting['time'],
-                'number': meeting['number'],
-                'participants': meeting['participants'],
-            })
-        return response
 
 
 class SkillList(ListAPIView):
