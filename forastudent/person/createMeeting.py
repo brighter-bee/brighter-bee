652e7c945b0cea2e4bd0a34b86d7af212af8d5be
z5164086 <z5164086@student.unsw.edu.au>
2020-08-09 19:02:52 +1000

    Updated so the meeting starts if anyone joins

---
+++ forastudent/person/createMeeting.py
@@ -0,0 +1,56 @@
+import sys
+import jwt
+import http.client
+import datetime
+import json
+
+
+
+# Go to 
+# Then get API Key, API Secret and insert below
+api_key = 'p-zpPVCrQMy4SnOlAV9BGw'
+api_sec = '8NWGe7uCQd8574r8EKpZJi3nrNRm3UPezoY7'
+
+
+payload = {
+'iss':api_key,
+'exp': datetime.datetime.now() + datetime.timedelta(hours=2)
+}
+
+jwt_encoded = str(jwt.encode(payload, api_sec), 'utf-8')
+
+conn = http.client.HTTPSConnection("api.zoom.us")
+headers = {
+'authorization': "Bearer %s" % jwt_encoded,
+'content-type': "application/json"
+}
+
+
+
+def createMeeting(start_time, duration, topic):
+    data = json.dumps({
+      "duration": duration,
+      "start_time": start_time,
+      "topic": topic,
+      "settings" : {"join_before_host" : True}
+    })
+    print(data)
+    conn.request("POST", "/v2/users/z5164086@student.unsw.edu.au/meetings", body=data, headers=headers)
+    res = conn.getresponse()
+    response_string = res.read().decode('utf-8')
+    response_obj = json.loads(response_string)
+    print(response_obj)
+
+    return response_obj['id']
+    
+    
+def getMeeting(id): 
+    conn.request("GET", "/v2/meetings/" + str(id) + "/invitation", headers=headers)
+    res = conn.getresponse()
+    response_string = res.read().decode('utf-8')
+    response_obj = json.loads(response_string)
+    print(response_obj)
+    return response_obj['invitation']
+
+            
+#createMeeting("2021-08-30T22:00:00Z", 60, "TestTopic", ["jamesclark@outlook.com.au"])
