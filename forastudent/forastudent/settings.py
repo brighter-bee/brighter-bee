76c7ab75f9aed252db15966c1dbe3a14736dbb91
Yifan Ai <ayfallen@hotmail.com>
2020-08-11 22:51:31 +1000

    Refactor new code and freeze requirements

--- forastudent/forastudent/settings.py
+++ forastudent/forastudent/settings.py
@@ -131,7 +131,7 @@ REST_REGISTRATION = {
     'RESET_PASSWORD_VERIFICATION_ENABLED': False,
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
