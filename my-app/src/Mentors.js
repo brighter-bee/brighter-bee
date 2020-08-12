91e218d6bc3231664727fbaee52be6e44649b484
Yifan Ai <ayfallen@hotmail.com>
2020-08-12 18:58:51 +1000

    Add functionality for searching mentors

--- my-app/src/Mentors.js
+++ my-app/src/Mentors.js
@@ -3,13 +3,21 @@ import Typography from '@material-ui/core/Typography';
 import axios from "axios";
 import MentorCard from "./MentorCard";
 import Grid from "@material-ui/core/Grid";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from '@material-ui/icons/Search';
+import Paper from "@material-ui/core/Paper";
 
 class Mentors extends Component {
     constructor() {
         super();
         this.state = {
-            persons: []
+            persons: [],
+            all: [],
+            search: '',
         };
+        this.updateField = this.updateField.bind(this);
+        this.searchCard = this.searchCard.bind(this);
     }
 
     componentDidMount() {
@@ -18,8 +26,43 @@ class Mentors extends Component {
                 console.log(response);
                 this.setState({
                     persons: response.data,
+                    all: response.data,
                 });
             });
+    }
+
+    updateField(e) {
+        e.preventDefault();
+        const target = e.target;
+        const name = target.name;
+
+        let value = target.value;
+
+        this.setState({
+            [name]: value
+        });
+        this.searchCard();
+    }
+
+    searchCard() {
+        let keyword = this.state.search;
+        let all = this.state.all;
+        const persons = all.filter(person => {
+            let result = false;
+            if (person.name) {
+                result = result || person.name.includes(keyword);
+            }
+            if (person.location) {
+                result = result || person.location.includes(keyword);
+            }
+            if (person.desc) {
+                result = result || person.desc.includes(keyword);
+            }
+            return result;
+        });
+        this.setState({
+            persons: persons
+        });
     }
 
     render() {
@@ -28,6 +71,18 @@ class Mentors extends Component {
                 <Typography variant="h4" component="h4">
                     Meet Mentors
                 </Typography>
+                <br/>
+                <Paper component="form" style={{border: '1px', padding: '1px 10px', width: '300px'}}>
+                    <IconButton onClick={this.searchCard}>
+                        <SearchIcon/>
+                    </IconButton>
+                    <InputBase style={{width: '230px'}}
+                               placeholder="Search mentors"
+                               name="search"
+                               value={this.state.search}
+                               onChange={this.updateField}
+                    />
+                </Paper>
                 <Grid container spacing={3}>
                     {this.state.persons.map((person) =>
                         <Grid item xs={12} sm={6} md={4} lg={3} key={person.id} style={{marginTop: '1rem'}}>
